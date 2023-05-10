d36eb65724ea81aa9513ccb255564064f3a2016b
pgs2285 <wldnjs2284@naver.com>
2023-05-10 09:52:35 +0900

    Initial Commit

---
+++ c_basic/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  c_basic
+//
+//  Created by 박지성 on 2023/05/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
