ccfa778c43fb66f52294e248470e8c82da960c8c
pgs2285 <wldnjs2284@naver.com>
2023-05-30 16:01:10 +0900

    2023-05-30 참조 메모리 다시알아보기

---
+++ c_basic/2_modernCpp/02_01_operatorOverloading.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+
+using namespace std;
+
+class Vector{
+public:
+    float x;
+    float y;
+    float z;
+    Vector operator+(){
+        return *this;
+    }
+
+    Vector operator+(const Vector& v){
+        return Vector{x + v.x , y+v.y , z+ v.y};
+    }
+
+    Vector operator-(){
+        return Vector{-x, -y, -z};
+    }
+
+    Vector operator-(const Vector& v){
+        return Vector{x - v.x, y - v.y, z-v.z};
+    }
+
+    Vector operator*(const float& num){
+
+        return Vector{num * x, num * y, num * z};
+    }
+
+    Vector operator*(const Vector& v){
+        return Vector{v.x * x, v.y * y, v.z * z};
+    }
+
+    Vector operator/(const Vector& v){
+        return Vector{v.x / x, v.y / y, v.z / z};
+    }
+// 참조를 쓰지 않는경우
+//Vector a(1, 2, 3);
+//Vector b = --a;
+// 위 식의 주손 서로 다른걸 가리킬거다
+// 참조쓰면 같은값 가리키고
+    Vector& operator++(){
+        
+        ++x;
+        ++y;
+        ++z;
+        // cout << "감소연산자 증가주소" <<this << endl;
+        // cout << this << endl;
+        return *(this);
+    }
+
+    Vector& operator--(){
+        --x;
+        --y;
+        --z;
+        // cout << "감소연산자 반환주소" <<this << endl;
+        return *this;
+    }
+    Vector& operator--(int){ // 후위연산자의 규칙같은거임
+        Vector tmp = *this;
+        --(*this);
+        // cout << "감소연산자 반환주소" <<this << endl;
+        return tmp;
+    }
+    
+
+    void printVec(){
+        cout << "X : " << x << " Y : " << y << " Z : " << z << endl;
+    }
+};
+
+int main(){
+    Vector v0 = {1,2,3};
+    Vector v1 = {0,1,2};
+    Vector nums = {-1,5,-3};
+
+    Vector v2 = v0.operator+(v1); // + 연산자 오버로딩(이항)
+    Vector v3 = v1.operator-(); // - 연산자 오버로딩(단항)
+    Vector v4 = nums.operator+(); // + 연산자 오버로딩(단항)
+    Vector v5 = v0.operator-(v1); // - 연산자 오버로딩(이항)
+    Vector v6 = v0.operator*(3.0f); // * 연산자 오버로딩 (이항, float 받아서)
+    Vector v7 = v0.operator*(nums); // * 연산자 오버로딩 (이항, vec받아서)
+    Vector v8 = v0.operator/(v1);// / 연산자 오버로딩 (이항, vec받아서)
+    Vector v9 = v0.operator++(); // ++연산자 오버로딩 (단항, 후위연산자) 자기자신이 직접 올라감
+    Vector v10 = v0.operator--(); // --연산자 오버로딩 (단항, 후위연상자) 자기자신이 직접 올라감
+
+    Vector v11 = v0 + v1; // 이제 이런식으로 직접 식에 넣는거도 가능하다.
+    Vector v12 = ++v0;
+
+    // Vector v13 = ++v12;
+
+    // v2.printVec(); //  + 
+    // v3.printVec();
+    // v4.printVec();
+    // v5.printVec();
+    // v6.printVec();
+    // v7.printVec();
+    // v8.printVec();
+    // v9.printVec();
+    // v10.printVec();
+    // // v12.printVec();
+    // // v13.printVec();
+    // v12.printVec();
+
+    cout << &v12 << endl;
+    cout << &v0 << endl;
+    return 0;
+}
--- c_basic/2_modernCpp/tempCodeRunnerFile.cpp
+++ c_basic/2_modernCpp/tempCodeRunnerFile.cpp
@@ -1,1 +1,27 @@
-height * 100
+#include <iostream>
+
+using namespace std;
+
+class Vector{
+public:
+    float x,y,z;
+
+    Vector(float a,float b, float c) : x(a), y(b), z(c) {
+
+    }
+    
+    Vector& operator++(){
+        
+        return *this;
+    }
+};
+
+int main(){
+
+    Vector v1(1.0f,2.0f,3.0f);
+    Vector v2 = ++v1;
+    cout << &v1 <<endl;
+    cout << &v2 << endl;
+
+    return 0;
+}
