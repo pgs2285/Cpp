eb8c3b089415d0f11a00475016504ebc395b404d
pgs2285 <wldnjs2284@naver.com>
2023-08-04 10:49:12 +0900

    2023-08-04

--- c_basic/2_modernCpp/09_08_ModernC++_std::function.cpp
+++ c_basic/2_modernCpp/09_08_ModernC++_std::function.cpp
@@ -18,18 +18,27 @@ struct Func2{
     static int func(int i) { return i; }
 };
 
+struct Func3{
+    int func(int i) { return i; }
+};
 
 int main()
 {
 
     std::function<int(float)> f1;
 
-    foo<int(float)>();                          // output : FifE (function<int(float)>)
+    foo<int(float)>();                                  // output : FifE (function<int(float)>)
     Func func;
     std::function<int(float)> f2 =  Func(); 
     cout << f2(1.1f) << endl;
 
-    std::function<int(int)> f3 = Func2::func;   // static member function은 함수 포인터로 변환되어야 한다. 
-    cout << typeid(f3).name() << endl;  
+    std::function<int(int)> f3 = Func2::func;           // static member function은 함수 포인터로 변환되어야 한다. 
+    cout << typeid(f3).name() << endl; 
+    // non-static 함수 Func3를 함수포인터로 가져오려면 다음과 같다
+    int(Func3:: *f)(int) = &Func3::func;                // Func3::func의 주소를 가져온다.
+    cout << (Func3().*f)(3) << endl;                    // Func3::func의 주소를 호출한다. 
+
+    std::function<int(Func3&, int)> f4 = &Func3::func;  // Func3::func의 주소를 가져온다. function<반환형(this, parameter)> 이므로 Func3&를 넣어준다.
+
     return 0;
 }
