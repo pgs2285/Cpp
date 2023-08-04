d467cc1a0d8c68f1b5efb360dfe57d8bc6a1c4f1
pgs2285 <wldnjs2284@naver.com>
2023-08-04 10:56:30 +0900

    2023-08-04 cpp

--- c_basic/2_modernCpp/09_08_ModernC++_std::function.cpp
+++ c_basic/2_modernCpp/09_08_ModernC++_std::function.cpp
@@ -39,6 +39,8 @@ int main()
     cout << (Func3().*f)(3) << endl;                    // Func3::func의 주소를 호출한다. 
 
     std::function<int(Func3&, int)> f4 = &Func3::func;  // Func3::func의 주소를 가져온다. function<반환형(this, parameter)> 이므로 Func3&를 넣어준다.
-
+    std::function<int(int)> f5 = std::bind(&Func3::func, Func3(), std::placeholders::_1); // std::bind를 사용하면, Func3::func의 주소를 가져온다.
+                                                        // std::bind는 함수포인터 반환, placeholders는 parameter를 받는다, _1은 첫번째 parameter를 받는다. 
+    cout << f5(3) << endl;                              // 즉 Func3::func 의 주소를 가져와서, Func3()의 주소를 넣어주고, parameter를 넣어준다.
     return 0;
 }
