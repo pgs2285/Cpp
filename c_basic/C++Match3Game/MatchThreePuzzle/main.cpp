d5992514a25b161b724a3e73db980de774c41e6d
pgs2285 <wldnjs2284@naver.com>
2023-08-21 15:58:26 +0900

    2023-08-21 cpp Game

--- c_basic/C++Match3Game/MatchThreePuzzle/main.cpp
+++ c_basic/C++Match3Game/MatchThreePuzzle/main.cpp
@@ -1,21 +1,34 @@
 
-#include <QtWidgets/QApplication>       // Application
-#include <QtWidgets/QGraphicsScene>     // Object를 띄우기 위한 Scene
-#include <QtWidgets/QGraphicsRectItem>  // Item
-#include <QtWidgets/QGraphicsView>      // 화면 floating 하기 위함
+#include <QApplication>       // Application
+#include <QGraphicsScene>     // Object를 띄우기 위한 Scene
+#include <QGraphicsRectItem>  // Item
+#include <QGraphicsView>      // 화면 floating 하기 위함
+//#include <QDesktopWidget>             // qt6 부터 QScreen으로 대체되었다
+#include <QScreen>
+#include <QGraphicsPixmapItem>
+
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);         // Application
+    QScreen* screen = QGuiApplication::primaryScreen();
+    QRect geometry = screen->geometry();
+    // QGuiApplication의 primaryScreen()으로 주 화면을 얻은 다음, 해당 화면의 geometry() 메서드를 이용하여 기하학적 영역에 대한 정보를 QRect 타입의 변수인 geometry에 저장
     QGraphicsScene scene;               // 여기에 만든 오브젝트들이 담긴다.
+    scene.setSceneRect(geometry);       // 좌표기준을 primaryScreen으로 변경 없으면 0,0(가운데)가 기준이되버린다.
 
-    QGraphicsRectItem rect(0,0,300,100);// (x,y,width,height)
-    scene.addItem(&rect);               // pointer로 넘겨받는다. 복사방지
+    QString CatImgFilePath = QCoreApplication::applicationFilePath() + "Cat_Avatar_Rounded.png";
+    QPixmap pixmap(CatImgFilePath); // 문자 그대로 읽어오게 즉 /c등을 그냥 문자그대로 인식하게 R"()"을사용.
+    QGraphicsPixmapItem CatImg(pixmap);   // pixmap에서 로드한 img를 띄워주는 역할을 한다.
+    scene.addItem(&CatImg);
 
     // 현재 scene안에 rect가 포함되어 있으나 viewing역할은 없다. 이상태면 백그라운드로만 돌아감
+
     QGraphicsView view(&scene);         //
     view.showFullScreen();
     // 비로소 화면에 넘겨졌다. Cpp는 메모리 절약을 위해 주소값을 넘겨주는 경우가 많으므로 익숙해지자.
     
+
     return a.exec();
 }
